1b04d5ac88ac452010652f83f61095e7d0fc336e
Parashkar prajwal <prajwalparashkar100@gmail.com>
2023-12-03 13:56:04 +0530

    recipe card changes recipe page is now server component

--- services/recipeService/getAllRecipeService.ts
+++ services/recipeService/getAllRecipeService.ts
@@ -1,17 +1,23 @@
-import { IRecipe } from '@/types/IRecipe';
-import axios from 'axios';
-import { cache } from 'react';
+import { IRecipe } from "@/types/IRecipe";
+import axios from "axios";
+import { cache } from "react";
 interface IAllRecipeResponce {
-    isSuccess: boolean;
-    data: {
-      recipes: IRecipe[];
-      message: string;
-    };
+  isSuccess: boolean;
+  data: {
+    recipes: IRecipe[];
+    message: string;
+  };
+}
+const getAllRecipeService = cache(async (page:(string | string[] | 0)):Promise<IAllRecipeResponce> => {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  let responce;
+  try {
+    responce = await axios.get<IAllRecipeResponce>(`${baseUrl}/recipe/get?page=${page?page : 0}`);
+    return responce.data;
+  } catch (error) {
+    console.log(error);
+    return  Promise.reject(responce);
   }
-const getAllRecipeService =cache( async() => {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-    const responce = await axios.get<IAllRecipeResponce>(`${baseUrl}/recipe/get`)
-    return responce.data
-})
+});
 
-export default getAllRecipeService
+export default getAllRecipeService;
